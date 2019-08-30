df0833201679ba98ea4a5fb9c5f97c48fb827119
lyf@hucii.com <lyf@hucii.com>
2019-08-30 23:04:02 +0800

    role-list

--- src/app/routes/system/role/role-list/role-list.component.ts
+++ src/app/routes/system/role/role-list/role-list.component.ts
@@ -1,15 +1,166 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { NotificationService } from '@core';
+import { STColumn, STColumnButton, STData } from '@delon/abc';
+import { deepCopy } from '@delon/util';
+import { BaseComponent } from '@layout/base.component';
+import { StatusColumnBadge } from '@shared';
+import { IRoleService } from '@System';
+import { NzModalService } from 'ng-zorro-antd';
+
+const initialStatusSelected = [{ Text: '停用', Value: 2 }, { Text: '正常', Value: 1 }];
 
 @Component({
-  selector: 'emes-role-list',
+  // tslint:disable-next-line:component-selector
+  selector: 'zc-role-list',
   templateUrl: './role-list.component.html',
-  styles: []
+  styles: [],
 })
-export class RoleListComponent implements OnInit {
+export class RoleListComponent extends BaseComponent implements OnInit {
+  @ViewChild('f', { static: false }) f: NgForm;
+  @ViewChild('contentTip', { static: false }) contentTip: TemplateRef<any>;
+  @ViewChild('footerTip', { static: false }) footerTip: TemplateRef<any>;
+  loading = false;
+  nodes = [];
+  selectNodes = [];
+  initialRole = {
+    parentId: '',
+    name: '',
+    status: 1,
+    sortNo: 10,
+  };
+  role: any;
+  keyword = '';
+  columns: STColumn[] = [
+    { title: '角色名称', index: 'createId', default: '-' },
+    { title: '状态', index: 'status', type: 'badge', badge: StatusColumnBadge },
+    {
+      title: '操作',
+      buttons: [
+        {
+          text: '编辑',
+          icon: 'edit',
+          type: 'link',
+          click: (_record, modal) => {},
+        },
+        {
+          text: '禁用',
+          icon: 'close-circle',
+          type: 'del',
+          popTitle: '确认禁用吗？',
+          click: (_record, modal) => {},
+          iif: (item: STData, btn: STColumnButton, column: STColumn) => {
+            return true;
+          },
+        },
+        {
+          text: '启用',
+          icon: 'check-circle',
+          type: 'del',
+          popTitle: '确认禁用吗？',
+          click: (_record, modal) => {},
+          iif: (item: STData, btn: STColumnButton, column: STColumn) => {
+            return false;
+          },
+        },
+        {
+          icon: '删除',
+          type: 'del',
+          click: (record, _modal, comp) => {
+            this.delete(_modal);
+          },
+          iif: (item: STData, btn: STColumnButton, column: STColumn) => {
+            return true;
+          },
+        },
+      ],
+    },
+  ];
+  list = [];
 
-  constructor() { }
+  statusSelected: { Text: any; Value: any }[] = deepCopy(initialStatusSelected);
 
-  ngOnInit() {
+  isVisible = false;
+  title = '新增角色';
+
+  constructor(
+    injector: Injector,
+    private notifySrv: NotificationService,
+    private roleSrv: IRoleService,
+    private modalSrv: NzModalService,
+  ) {
+    super(injector);
   }
 
+  ngOnInit() {
+    this.role = this.initialRole;
+    this.getList();
+  }
+
+  refresh() {
+    this.getList();
+  }
+
+  reset() {
+    this.f.reset(this.initialRole);
+    this.handleCancel();
+  }
+
+  getList() {
+    this.roleSrv.query({ request: { name: '' } }).subscribe((x: any) => {
+      if (!x) {
+        return;
+      }
+      this.notifySrv.success();
+    });
+  }
+
+  add($event: any) {
+    this.role = this.initialRole;
+    this.isVisible = true;
+  }
+
+  edit($event: any) {
+    this.role = $event;
+    this.isVisible = true;
+  }
+
+  handleCancel = () => {
+    this.isVisible = false;
+  };
+
+  handleOk = () => {
+    this.isVisible = false;
+  };
+
+  save($event: any) {
+    // this.loading = true;
+    if (this.role.id === undefined) {
+      this.roleSrv.create({ request: this.role }).subscribe(x => {
+        this.notifySrv.success();
+        this.getList();
+        this.reset();
+        this.loading = false;
+      });
+    } else {
+      this.roleSrv.update(this.role).subscribe(x => {
+        this.notifySrv.success();
+        this.getList();
+        this.reset();
+        this.loading = false;
+      });
+    }
+  }
+
+  delete($event: any) {
+    if (this.role.id !== undefined) {
+      this.roleSrv.delete({ request: { id: this.role.id } }).subscribe(x => {
+        this.notifySrv.success();
+        this.reset();
+        this.getList();
+      });
+    } else {
+      this.notifySrv.info('请选择需要删除的记录！');
+    }
+  }
 }
--- src/style-icons.ts
+++ src/style-icons.ts
@@ -1,11 +1,21 @@
 // Custom icon static resources
 
 import {
+  BulbOutline,
+  CheckCircleOutline,
+  CloseCircleOutline,
+  ExceptionOutline,
+  InfoOutline,
+  LinkOutline,
+  ProfileOutline,
+} from '@ant-design/icons-angular/icons';
+
+export const ICONS = [
   InfoOutline,
   BulbOutline,
   ProfileOutline,
   ExceptionOutline,
   LinkOutline,
-} from '@ant-design/icons-angular/icons';
-
-export const ICONS = [InfoOutline, BulbOutline, ProfileOutline, ExceptionOutline, LinkOutline];
+  CloseCircleOutline,
+  CheckCircleOutline,
+];
