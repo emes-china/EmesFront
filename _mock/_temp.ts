fc38fce2ecb70a23e43d527c5117a68da3e18522
anber <shuangyan_m@hotmail.com>
2019-08-30 22:43:01 +0800

    mock

---
+++ _mock/_temp.ts
@@ -0,0 +1,5 @@
+export const Temp = {
+  '//api/user/authentication': JSON.parse(
+    JSON.stringify({ entity: '123', isSucceed: false, message: '', statusCode: 200 }),
+  ),
+};
--- _mock/index.ts
+++ _mock/index.ts
@@ -1,2 +1,3 @@
 export * from './_user';
 export * from './_chart';
+export * from './_temp';
